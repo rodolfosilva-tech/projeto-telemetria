d15d93bdda15cbf51ef6ac2fbc656fea00e27d5f
Rodolfo Silva <rodolfo.silva@technook.com.br>
2026-09-03 09:14:25 -0300

    feat: substituir código padrão por rotas de dashboard e imóveis, atualizar título do documento e configurar estilos globais de fonte

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Imoveis } from './imoveis/imoveis';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Dashboard, Imoveis],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
